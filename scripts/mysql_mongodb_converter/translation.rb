968572c92e6d96f040247be713e2fb3f72015307
Arkka Dhiratara <arkka.d@gmail.com>
2016-11-10 23:34:58 +0100

    Preserve legacy standarized BPS `id`, as `code`

--- scripts/mysql_mongodb_converter/translation.rb
+++ scripts/mysql_mongodb_converter/translation.rb
@@ -1,23 +1,35 @@
 table "districts" do
-	column "id", :key, :as => :integer
-	column "regency_id", :integer, :references => "regencies"
+	column "id", :key, :as => :integer, :rename_to => "code"
+	column "regency_id", :integer, :references => "regencies", :rename_to => "regency"
 	column "name", :string
+
+	before_save do |row|                                    
+    	row.code = row.delete('pre_mongified_id')
+  	end      
 end
 
 table "provinces" do
 	column "id", :key, :as => :integer
 	column "name", :string
+	before_save do |row|                                    
+    	row.code = row.delete('pre_mongified_id')
+  	end     
 end
 
 table "regencies" do
-	column "id", :key, :as => :integer
-	column "province_id", :integer, :references => "provinces"
+	column "id", :key, :as => :integer, :rename_to => "code"
+	column "province_id", :integer, :references => "provinces", :rename_to => "province"
 	column "name", :string
+	before_save do |row|                                    
+    	row.code = row.delete('pre_mongified_id')
+  	end        
 end
 
 table "villages" do
-	column "id", :key, :as => :integer
-	column "district_id", :integer, :references => "districts"
+	column "id", :key, :as => :integer, :rename_to => "code"
+	column "district_id", :integer, :references => "districts", :rename_to => "district"
 	column "name", :string
+	before_save do |row|                                    
+    	row.code = row.delete('pre_mongified_id')
+  	end       
 end
-
