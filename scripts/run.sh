fd09487aa5ef635449c6379a59148b892aac8107
Fery Wardiyanto <ferywardiyanto@gmail.com>
2017-06-29 05:51:20 +0700

    Add optional argument for script: - h: for database host - u: for database user
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
 DB_NAME="indonesia"
+DB_HOST="mysql"
+DB_USER="root"
 
 TMP_DIR="/tmp/indonesia"
+
+SCRIPT_DIR=`dirname $0`
+CSV_DIR=`realpath ${SCRIPT_DIR}/../csv`
+
+while getopts 'h:u:' opts; do
+    case $opts in
+        h) DB_HOST=$OPTARG ;;
+        u) DB_USER=$OPTARG ;;
+        *) error 'Unexpected argument' ;;
+    esac
+done
 
 mkdir -p ${TMP_DIR}
 
@@ -15,26 +28,26 @@ for key in 'a' 'i' 'u' 'e' 'o'; do
 	fi
 
 	echo "Parsing ${key}"
-	./mdf_parser.py ${TMP_DIR} ${DB_NAME}-${key}.html ${key}
+	$SCRIPT_DIR/mdf_parser.py ${TMP_DIR} ${DB_NAME}-${key}.html ${key}
 done
 unset key
 
 for area in "provinces" "regencies" "districts" "villages"; do
 	echo "Combining ${area} into ../csv/${area}.csv"
-	cat ${TMP_DIR}/${area}-*.csv | sort | uniq > ../csv/${area}.csv
+	cat ${TMP_DIR}/${area}-*.csv | sort | uniq > $CSV_DIR/${area}.csv
 done
 
 echo "Writing MySQL dump file"
-./mdf_mysql_converter.py ../csv/provinces.csv ../csv/regencies.csv ../csv/districts.csv ../csv/villages.csv > ../mysql/${DB_NAME}.sql
+$SCRIPT_DIR/mdf_mysql_converter.py $CSV_DIR/provinces.csv $CSV_DIR/regencies.csv $CSV_DIR/districts.csv $CSV_DIR/villages.csv > $SCRIPT_DIR/../mysql/$DB_NAME.sql
 
 # rm -rf $TMP_DIR
 
 echo "Testing MySQL dump..."
 echo "> Creating database"
-mysql -u root -h mysql -e "CREATE DATABASE IF NOT EXISTS indonesia;"
+mysql -u $DB_USER -h $DB_HOST -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 echo "> Importing database"
-mysql -u root -h mysql indonesia < ../mysql/indonesia.sql
+mysql -u $DB_USER -h $DB_HOST $DB_NAME < $SCRIPT_DIR/../mysql/$DB_NAME.sql
 echo "> Statistics"
-mysql -u root -h mysql -t indonesia < ./statistics.sql
+mysql -u $DB_USER -h $DB_HOST -t $DB_NAME < $SCRIPT_DIR/statistics.sql
 
 echo "Done."
