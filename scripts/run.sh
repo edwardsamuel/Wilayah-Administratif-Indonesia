1a2b7c02cb9320acdb513100a808dc1be062cc9a
Fery Wardiyanto <ferywardiyanto@gmail.com>
2017-06-29 20:57:35 +0700

    Add error exit when passwing invalid option and add option to purge tmp dir before run the script
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,14 +8,30 @@ TMP_DIR="/tmp/indonesia"
 
 SCRIPT_DIR=`dirname $0`
 CSV_DIR=`realpath ${SCRIPT_DIR}/../csv`
+SQL_DIR=`realpath ${SCRIPT_DIR}/../mysql`
 
-while getopts 'h:u:' opts; do
+purge=false
+
+while getopts ':h:u:p' opts; do
     case $opts in
         h) DB_HOST=$OPTARG ;;
         u) DB_USER=$OPTARG ;;
-        *) error 'Unexpected argument' ;;
+        p) purge=true ;;
+        \?)
+            echo "Invalid option -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
     esac
 done
+
+if $purge = true; then
+    echo 'purging tmp dir'
+    rm -rf $TMP_DIR
+fi
 
 mkdir -p ${TMP_DIR}
 
@@ -33,20 +49,18 @@ done
 unset key
 
 for area in "provinces" "regencies" "districts" "villages"; do
-	echo "Combining ${area} into ../csv/${area}.csv"
+	echo "Combining ${area} into ${area}.csv"
 	cat ${TMP_DIR}/${area}-*.csv | sort | uniq > $CSV_DIR/${area}.csv
 done
 
 echo "Writing MySQL dump file"
-$SCRIPT_DIR/mdf_mysql_converter.py $CSV_DIR/provinces.csv $CSV_DIR/regencies.csv $CSV_DIR/districts.csv $CSV_DIR/villages.csv > $SCRIPT_DIR/../mysql/$DB_NAME.sql
-
-# rm -rf $TMP_DIR
+$SCRIPT_DIR/mdf_mysql_converter.py $CSV_DIR/provinces.csv $CSV_DIR/regencies.csv $CSV_DIR/districts.csv $CSV_DIR/villages.csv > $SQL_DIR/$DB_NAME.sql
 
 echo "Testing MySQL dump..."
 echo "> Creating database"
 mysql -u $DB_USER -h $DB_HOST -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 echo "> Importing database"
-mysql -u $DB_USER -h $DB_HOST $DB_NAME < $SCRIPT_DIR/../mysql/$DB_NAME.sql
+mysql -u $DB_USER -h $DB_HOST $DB_NAME < $SQL_DIR/$DB_NAME.sql
 echo "> Statistics"
 mysql -u $DB_USER -h $DB_HOST -t $DB_NAME < $SCRIPT_DIR/statistics.sql
 
